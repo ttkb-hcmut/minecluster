d82f2ef66eb7cfdaa4810838031c0febdf672979
LeCongMinhKhang <khang.lecongminh@gmail.com>
2026-07-08 17:42:27 +0700

    cli 3 laying out the general Usecases of the nodes stuff

--- lib/cli.ex
+++ lib/cli.ex
@@ -44,7 +44,7 @@ defmodule Command do
        "\t" <> (case k do
         :"" -> "<input>"
         _ -> k |> Atom.to_string
-      end) <> " => " <> (ctx |> Map.get(:c) |> Map.get(k) |> Map.get(:i,"No information")) |> IO.puts
+      end) <> " => " <> (ctx |> Map.get(:c, %{}) |> Map.get(k) |> Map.get(:i,"No information")) |> IO.puts
     end
   end
 end
@@ -59,14 +59,23 @@ defmodule Cli do
         i: "Configure stuff",
         a: fn a -> Command.prompt(a) end,
         c: %{
-          address:  %{
-            i: "Address of self",
+          node_address:  %{
+            i: "Address of self node used when connecting with other nodes",
             a:  fn a -> Command.prompt(a) end,
             c:  %{
               "": %{
-                i: "Value",
-                a: fn a -> Command.captured(a) end,
-                c: %{}
+                i: "String in the form of <name>@<ip address>",
+                a: fn a -> Command.captured(a) end
+              }
+            }
+          },
+          node_cookie:  %{
+            i: "Default secret node cookie used when connecting to nodes with the same cookie",
+            a:  fn a -> Command.prompt(a) end,
+            c:  %{
+              "": %{
+                i: "String",
+                a: fn a -> Command.captured(a) end
               }
             }
           }
@@ -76,18 +85,66 @@ defmodule Cli do
         i: "Node commands",
         a: fn a -> Command.prompt(a) end,
         c: %{
-          start:  %{i: "Starts Node address defined in config",
-                    a: fn _ -> Command.foo() end,
-                    c: %{}
+          start:  %{
+            i: "Starts Node address defined in config, or with the arg provided after it",
+            a: fn _ -> Command.foo() end,
+            c: %{
+              "": %{
+                i: "Captured address for Node starting",
+                a: fn a -> Command.captured(a) end
+              }
+            }
+          },
+          connect:  %{
+            i: "Connect Node to the saved group or address with the cookie provided by Config or the arg following",
+            a: fn a -> Command.prompt(a) end,
+            c: %{
+              group: %{
+                i: "Try all nodes in group with the cookie provided by Config or the arg following",
+                a: fn a -> Command.prompt(a) end,
+                c: %{
+                  "": %{
+                    i: "Group name",
+                    a: fn a -> Command.captured(a) end,
+                    c: %{
+                      "": %{
+                        i: "Cookie override",
+                        a: fn a -> Command.captured(a) end
+                      }
+                    }
                   }
+                }
+              },
+              "": %{
+                i: "Destination node address with the cookie provided by Config or the arg following",
+                a: fn a -> Command.captured(a) end,
+                c: %{
+                  "": %{
+                    i: "Cookie override",
+                    a: fn a -> Command.captured(a) end
+                  }
+                }
+              }
+            }
+          },
+          make_group: %{
+            i: "Adds all nodes currently connected to the group for when connecting node to group, Node must have already been started",
+            a: fn _ -> Command.foo() end
+          },
+          disconnect: %{
+            i: "Disconnects fromm current Node network",
+            a: fn _ -> Command.foo() end
+          },
+          stop: %{
+            i: "Stops node",
+            a: fn _ -> Command.foo() end
+          }
         }
       },
-      connect_to_group:   %{i: "capturing command arg",
-              a:  fn a -> Command.captured(a) end
-            },
-      exit: %{i: "exit the cli",
-              a:  fn _ -> Command.exitCli() end
-            }
+      exit: %{
+        i: "exit the cli",
+        a: fn _ -> Command.exitCli() end
+      }
     }
   }
   end
