994105e13028e9ac00d459d40f1decff3adcb46f
Kinten Le <kinten108101@protonmail.com>
2026-07-29 15:03:54 +0700

    gui fudget updated hello world

--- gui/fudget/app/Main.hs
+++ gui/fudget/app/Main.hs
@@ -4,4 +4,13 @@ import Fudgets
 
 main :: IO ()
 main =
-  fudlogue (shellF "Hello" $ labelF "Hello world!")
+  fudlogue (shellF "Hello" (
+    stringF >==< absF (stateSP "") >==< la
+  ))
+
+la :: F Click (String -> String)
+la = const (++"wow") >^=< buttonF "sd"
+
+stateSP :: state -> SP (state->state) state
+stateSP =
+  mapAccumlSP (\ acc f -> let newv = f acc in (newv, newv))
