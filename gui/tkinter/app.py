b4f246b5320c55b389e39acd7375d60a19171394
Le Khang <khang.lecongminh@gmail.com>
2026-09-10 16:41:55 +0700

    tkinter gui prototype (run gui/tkinter/app.py)

--- gui/tkinter/app.py
+++ gui/tkinter/app.py
@@ -1,20 +1,112 @@
 import tkinter as tk
-from .components import model
+from tkinter import ttk
+from components import model
+
+
+class App(tk.Tk):
+  def __init__(self,controller):
+    super().__init__()
+    self.title("Minecluster")
+    self.geometry("1000x500") 
+    self.minsize(500,250) 
+    self.columnconfigure(index=(0),weight=1)
+    self.rowconfigure(index=(1),weight=1)
+    
+    self.backgroundColor = ttk.Label(self,background= "#ffffff").grid(row=0, column=0, rowspan=2, sticky="nsew")
+    self.tabsBar = TabsBar(self,controller)
+    self.main = Main(self,controller)
+    self.mainloop()
+
+
+class Main(ttk.Frame):
+  def __init__(self,parent,controller):
+    super().__init__(parent)
+    self.grid(row=1, column=0)
+    self.columnconfigure(index=(0),weight=1)
+    self.rowconfigure(index=(1),weight=1)
+    # self.backgroundColor = ttk.Label(self,background= "#ff0000").pack(expand= True, fill = "both")
+
+    mainHome = MainTab(self,controller,"home")
+    mainGroup = MainTab(self,controller,"group")
+    mainConfig = MainTab(self,controller,"config")
+    
+class MainTab(ttk.Frame):
+  def __init__(self,parent,controller,title):
+    super().__init__(parent)
+    self.grid(row=1, column=0)
+    print(f"current tab is: {controller.get("currentTab")}")
+    if controller.get("currentTab") != title:
+      self.grid_remove()
+    else:
+      self.grid()
+
+    self.title = title
+    self.label = ttk.Label(self,text=f"Menu of {title}")
+    self.label.grid(row=1, column=0, sticky="nsew")
+    controller.subscribe("currentTab", self.showHideMenu)
+
+
+  def showHideMenu(self,data):
+    if data == self.title:
+      self.grid()
+    else:
+      self.grid_remove()
+
+class TabsBar(ttk.Frame):
+  def __init__(self,parent,controller):
+    super().__init__(parent)
+    self.grid(row=0, column=0, sticky="nsew")
+
+    self.button_style = ttk.Style()
+    self.button_style.configure(
+      'button_active.TButton', 
+      foreground='#000000',   # Button body color
+      background='#FF00FF',   # Button body color
+      padding = 5,
+    )
+    self.button_style.configure(
+      'button_inactive.TButton', 
+      foreground ='#888888',   # Button body color
+      background ='#dddddd',   # Button body color
+      padding = 0
+    )
+
+
+    self.home_button    = ttk.Button(self,text="Home", style='button_active.TButton', padding=5,
+                            command = lambda: controller.set("currentTab","home"))
+    self.group_button   = ttk.Button(self,text="Group", style='button_inactive.TButton',
+                            command = lambda: controller.set("currentTab","group"))
+    self.config_button  = ttk.Button(self,text="Config", style='button_inactive.TButton',
+                            command = lambda: controller.set("currentTab","config"))
+    self.home_button.pack(side='left')
+    self.group_button.pack(side='left')
+    self.config_button.pack(side='left')
+
+    # controller.subscribe("currentTab", lambda: self.configure_buttons())
+    controller.subscribe("currentTab", self.configure_buttons)
+
+  def configure_buttons(self,data):
+    self.home_button.configure(style='button_inactive.TButton',padding=0)
+    self.group_button.configure(style='button_inactive.TButton',padding=0)
+    self.config_button.configure(style='button_inactive.TButton',padding=0)
+    match data:
+      case "home":
+        self.home_button.configure(style='button_active.TButton',padding=5)
+      case "group":
+        self.group_button.configure(style='button_active.TButton',padding=5)
+      case "config":
+        self.config_button.configure(style='button_active.TButton',padding=5)
+
 def add1tonum(num):
   num.set(num.get() + 1)
 
-def window(root,ma):
-  num = tk.IntVar(value=0)
-  label = tk.Label(root, textvariable=num, font=("Arial", 16))
-  button = tk.Button(root, text = "balls", command = lambda: ma.action("changeTab",[thedata]))
-  label.pack(pady=20)
-  button.pack(pady=20)
+# def window(root,ma):
+#   num = ttk.IntVar(value=0)
+#   label = ttk.Label(root, textvariable=num, font=("Arial", 16))
+#   button = ttk.Button(root, text = "balls", command = lambda: ma.action("changeTab",[thedata]))
+#   label.pack(pady=20)
+#   button.pack(pady=20)
 
 if __name__ == "__main__":
   controller = model.Updater()
-  root = tk.Tk()
-  root.title("Mineclusterer")
-  root.geometry("1000x500")
-  window(root)
-  root.mainloop()
-  print("hello world")
+  app = App(controller)
--- gui/tkinter/components/model.py
+++ gui/tkinter/components/model.py
@@ -1,36 +1,38 @@
-from dataclasses import dataclass, fields
+from dataclasses import dataclass, field, fields
 import threading
 @dataclass
 class Model:
   # GUI specific data fields ====================
-  currentTab: str
+  currentTab: str = "home"
 
   # Application data fields =====================
   ## config/all
-  configAll: dict
+  configAll: dict = field(default_factory=dict)
   ## list
-  nodeList: list
-  central: list
-  host: list
-  online: list
+  nodeList: list = field(default_factory=list)
+  central: list = field(default_factory=list)
+  host: list = field(default_factory=list)
+  online: list = field(default_factory=list)
   ## group/list
-  groupList: dict
+  groupList: dict = field(default_factory=dict)
   ## group/status
-  groupStatusName: str
-  groupStatusConnections: list
-  groupStatusCookie: str
+  groupStatusName: str = ""
+  groupStatusConnections: list = field(default_factory=list)
+  groupStatusCookie: str = ""
 
 class Updater:
   def __init__(self):
     self._lock = threading.Lock()
     self.model = Model()
+    print(f"started up with {getattr(self.model, "currentTab")}")
     self.subscribers = {}
     self.modelFields = list(map(lambda e: e.name, fields(Model)))
     for field in self.modelFields:
       self.subscribers[field] = []
 
   def subscribe(self,field,func):
-    self.subscribers[field] += [func]
+    self.subscribers[field] = self.subscribers[field] + [func]
+    print(f"{field} -> {func}")
 
   def sendFetch(self,api):
     #send data
@@ -41,8 +43,8 @@ class Updater:
       if field in self.modelFields:
         setattr(self.model, field, data)
 
-        for subscriber in self.subscribers[field]:
-          subscriber()
+        for subscriber in self.subscribers[field]: 
+          subscriber(data)    
       else:
         raise ValueError(f"Unknown datafield '{field}'")
     
@@ -53,13 +55,15 @@ class Updater:
           setattr(self.model, field, data)
 
           for subscriber in self.subscribers[field]:
-            subscriber()
+            subscriber(data)
         else:
           raise ValueError(f"Unknown datafield '{field}'")
 
   def get(self,field):
     with self._lock:
       if field in self.modelFields:
-          getattr(self.model, field)
+        res = getattr(self.model, field)
+        print(f"Getting field: {field} = {res}")
+        return res
       else:
         raise ValueError(f"Unknown datafield '{field}'")
